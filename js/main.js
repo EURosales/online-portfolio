de626de90159dc9c1d318ee33de40aa51bee5536
EURosales <eduardo_rosales26@outlook.com>
2021-06-29 01:10:46 -0600

    responsive menu bar and home section

--- js/main.js
+++ js/main.js
@@ -5,3 +5,12 @@ $(window).on("scroll", function () {
         $("nav").removeClass("black");
     }
 });
+
+function updatemenu() {
+    if (document.getElementById('responsive-menu').checked == true) {
+        document.getElementById('menu').style.borderBottomRightRadius = '0';
+        document.getElementById('menu').style.borderBottomLeftRadius = '0';
+    } else {
+        document.getElementById('menu').style.borderRadius = '5px';
+    }
+}
